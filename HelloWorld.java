7d6f04d27cc84b4358be41224e918a3f95d5361d
Ali Ahmad <142441350+holialli@users.noreply.github.com>
2026-04-23 13:29:18 +0500

    Initializing Java repo

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("Hello! Jenkins Lab 11 is Live.");
+        System.out.println("Build Successful for Ali Ahmad (i232050)");
+        System.out.println("---------------------------------------");
+    }
+}
